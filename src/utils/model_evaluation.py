beeff35a186300f71163ab0c71358a9359dab0da
Denys Herasymuk <herasymuk@ucu.edu.ua>
2022-12-23 12:04:12 +0200

    Added torch.no_grad

--- src/utils/model_evaluation.py
+++ src/utils/model_evaluation.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 from tqdm import tqdm
 from ml_metrics.average_precision import mapk
@@ -16,32 +17,33 @@ def evaluate(model, model_name, models_metrics_df, train_dataset, val_dataset, a
     class_predicted_lst = []
     superclass_actual_lst = []
     superclass_predicted_lst = []
-    for idx, (img, true_class_id, true_superclass_id) in enumerate(tqdm(val_dataset)):
-        if img is None:
-            continue
-        embedding = get_embedding(model, img)
-        # neighbours = annoy_index.get_nns_by_vector(embedding, 6)[1:]  # get top 5 closest
-        neighbours = annoy_index.get_nns_by_vector(embedding, metrics_k)  # get top k closest
+    with torch.no_grad():
+        for idx, (img, true_class_id, true_superclass_id) in enumerate(tqdm(val_dataset)):
+            if img is None:
+                continue
+            embedding = get_embedding(model, img)
+            neighbours = annoy_index.get_nns_by_vector(embedding, metrics_k)  # get top k closest
 
-        pred_class_ids = []
-        pred_superclass_ids = []
-        for i in range(len(neighbours)):
-            _, pred_class_id, pred_super_class_id = train_dataset[neighbours[i]]
-            pred_class_ids.append(pred_class_id)
-            pred_superclass_ids.append(pred_super_class_id)
+            pred_class_ids = []
+            pred_superclass_ids = []
+            for i in range(len(neighbours)):
+                _, pred_class_id, pred_super_class_id = train_dataset[neighbours[i]]
+                pred_class_ids.append(pred_class_id)
+                pred_superclass_ids.append(pred_super_class_id)
 
-        # Save results to compute accuracy
-        class_true_lst.append(true_class_id)
-        class_preds_lst.append(pred_class_ids[0])
-        superclass_true_lst.append(true_superclass_id)
-        superclass_preds_lst.append(pred_superclass_ids[0])
-        # Save results to compute mAP@k
-        class_actual_lst.append([true_class_id])
-        class_predicted_lst.append(pred_class_ids)
-        superclass_actual_lst.append([true_superclass_id])
-        superclass_predicted_lst.append(pred_superclass_ids)
+            # Save results to compute accuracy
+            class_true_lst.append(true_class_id)
+            class_preds_lst.append(pred_class_ids[0])
+            superclass_true_lst.append(true_superclass_id)
+            superclass_preds_lst.append(pred_superclass_ids[0])
+            # Save results to compute mAP@k
+            class_actual_lst.append([true_class_id])
+            class_predicted_lst.append(pred_class_ids)
+            superclass_actual_lst.append([true_superclass_id])
+            superclass_predicted_lst.append(pred_superclass_ids)
 
     precision = 4
+    # Compute Accuracy
     class_id_accuracy = accuracy_score(np.array(class_true_lst), np.array(class_preds_lst))
     superclass_id_accuracy = accuracy_score(np.array(superclass_true_lst), np.array(superclass_preds_lst))
     class_id_accuracy = np.round(class_id_accuracy, precision)
@@ -50,6 +52,7 @@ def evaluate(model, model_name, models_metrics_df, train_dataset, val_dataset, a
     print(f'[{model_name}] Accuracy for class_id: {class_id_accuracy}')
     print(f'[{model_name}] Accuracy for superclass_id: {superclass_id_accuracy}')
 
+    # Compute mAP@k
     class_id_mapk = mapk(class_actual_lst, class_predicted_lst, metrics_k)
     superclass_id_mapk = mapk(superclass_actual_lst, superclass_predicted_lst, metrics_k)
     class_id_mapk = np.round(class_id_mapk, precision)
